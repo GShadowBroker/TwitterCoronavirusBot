ec1fce9366b352aca3446cc9b56778dae2a15fdc
Gledyson <gledysonferreira@gmail.com>
2020-04-10 17:16:27 -0400

    adicionado momentjs para exibição de tempo da última atualização, bem como outras alterações menores.

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require('express');
 const Twit = require('twit');
 const config = require('./config');
 const axios = require('axios');
+const moment = require('moment');
+require('moment/locale/pt-BR');
 const port = 3000;
 
 const app = express();
 
 let T = new Twit(config);
-let termsToTrack = ['@coronavirusbrbot', '@CoronavirusBrBot', '@CoronavirusBRBot'];
 
 T.get('account/verify_credentials', {
     include_entities: false,
@@ -23,9 +24,12 @@ function onAuthenticated (err) {
     } else {
         console.log('Authentication successful!');
 
-        let stream = T.stream('statuses/filter', { track: termsToTrack, tweet_mode: 'extended'});
+        let stream = T.stream('statuses/filter', { track: config.termsToTrack, tweet_mode: 'extended'});
         stream.on('tweet', (tweet) => {
-            if (!tweet.retweeted_status && isTweetExactMatch(tweet.text) && tweet.user.screen_name !== 'Não tem biscoito') {
+            if (!tweet.retweeted_status
+                && isTweetExactMatch(tweet.text)
+                && tweet.user.screen_name !== 'Não tem biscoito') {
+
                 console.log(tweet.text);
                 sendReply(tweet);
             }
@@ -35,21 +39,37 @@ function onAuthenticated (err) {
 
 function sendReply (tweet) {
     let screenName = tweet.user.screen_name;
-    axios.get('https://covid19-brazil-api.now.sh/api/report/v1/brazil')
-        .then(response => {
-            let msg = `@${screenName} No Brasil há atualmente ${response.data.data.confirmed} casos confirmados, ${response.data.data.deaths} mortes e ${response.data.data.recovered} recuperados.`;
-            
-            T.post('statuses/update', {
-                in_reply_to_status_id: tweet.id_str,
-                status: msg
-            }, onTweeted);
-        })
-        .catch(err => console.log(err));
+    let uf = tweet.text.trim().slice(18, 20).toUpperCase();
+
+    if (config.states.includes(uf)) {
+        axios.get(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${uf}`)
+            .then(response => {
+                let lastUpdate = moment(response.data.datetime).fromNow();
+                let msg = `@${screenName} Em ${response.data.state} há atualmente ${response.data.cases} casos confirmados, ${response.data.suspects} casos suspeitos e ${response.data.deaths} mortes. Última vez atualizado: ${lastUpdate}.`;
+                T.post('statuses/update', {
+                    in_reply_to_status_id: tweet.id_str,
+                    status: msg
+                }, onTweeted);
+            })
+            .catch(err => console.log(err));
+    } else {
+        axios.get('https://covid19-brazil-api.now.sh/api/report/v1/brazil')
+            .then(response => {
+                let lastUpdate = moment(response.data.data.updated_at).fromNow();
+                let msg = `@${screenName} No Brasil há atualmente ${response.data.data.confirmed} casos confirmados, ${response.data.data.deaths} mortes e ${response.data.data.recovered} recuperados. Última vez atualizado: ${lastUpdate}.`;
+                
+                T.post('statuses/update', {
+                    in_reply_to_status_id: tweet.id_str,
+                    status: msg
+                }, onTweeted);
+            })
+            .catch(err => console.log(err));
+    }
 }
 
 function isTweetExactMatch (text) {
     text = text.toLowerCase();
-    return termsToTrack.some(term => text.includes(term));
+    return config.termsToTrack.some(term => text.includes(term));
 }
 
 function sendTweet (text) {
--- config.js
+++ config.js
@@ -4,5 +4,7 @@ module.exports = {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    termsToTrack: ['@coronavirusbrbot', '@CoronavirusBrBot', '@CoronavirusBRBot', '@CoronavirusBrB1'],
+    states: ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
 };
